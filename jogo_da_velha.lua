e29e0d0669065d8892aa2460c831778a0a79052c
Raquel de Sá <silva.raquel.de.sa@gmail.com>
2023-12-19 22:18:09 -0400

    feat: adicionando fluxo do jogo

--- jogo_da_velha.lua
+++ jogo_da_velha.lua
@@ -1,5 +1,18 @@
 utils = require("utils")
 
+-- FLUXO do JOGO
+-- 1. printInicio()
+-- 2. printEstadoAtualJogo()
+-- 3. ler coordenada jogador 1
+-- 4. printEstadoAtualJogo()
+-- 5. ler coordenada jogador 2
+-- 6. printEstadoAtualJogo()
+-- 7. verificaGanhador()
+-- 8. repete tudo ou finaliza
+
+-- futura feature -> criar um arquivo e registrar os jogos e pontuações
+
+
 jogo = 1
 
 -- print(cerquilha)
