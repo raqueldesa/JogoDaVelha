ef01bf6c36da12b0aaa2c43be07f9041fdf5fe4b
Raquel <raquel.sa.2dinnovation@gmail.com>
2023-12-20 13:18:14 -0400

    feat: lerCoordenadas e verificaValoresIguais

--- jogo_da_velha.lua
+++ jogo_da_velha.lua
@@ -18,15 +18,26 @@ jogo = 1
 -- print(cerquilha)
 
 local matriz = {
-    {0, 0, 0},
+    {1, 1, 1},
     {0, 1, 0},
-    {0, 0, 2}
+    {0, 1, 0}
 }
+
+print(type(matriz[1]))
+
 -- 1 == X
 -- 2 == O
 
-printInicio()
+-- printInicio()
 
--- printJogo(matriz)
+-- printEstadoAtualJogo(matriz)
+-- matriz = lerCoordenadas(1, matriz)
 
--- print("Jogador X ")
+-- ganhador = verificaGanhador(matriz)
+
+-- printEstadoAtualJogo(matriz)
+
+print(verificaValoresIguais(matriz[1]))
+
+-- ideia: percorrer a matriz e adicionar a uma 'lista' os valores das colunas e diagonais, para serem chamadas pela funcao verificaValoresIguais()
+
--- utils.lua
+++ utils.lua
@@ -20,6 +20,12 @@ cerquilha1 = [[
     |   |
 ]]
 
+function pausaELimpa()
+    print()
+    os.execute("pause")
+    os.execute("cls")
+end
+
 function printInicio()
     print()
     print("Bem vindo ao Jogo da Velha da Raquel :)")
@@ -28,39 +34,33 @@ function printInicio()
     print("Reginha:")
     print("Jogador 1 = X")
     print("Jogador 2 = O")
-    print()
-    os.execute("pause")
-    os.execute("cls")
+    
+    pausaELimpa()
     
     print("Para escolher onde quer jogar, digite dois numeros que indicam linha e coluna de uma matriz 3x3")
     print("Exemplo:")
     print("Jogo antes:")
     print(cerquilha0)
-    print()
-    os.execute("pause")
-    os.execute("cls")
+
+    pausaELimpa()
 
     print("Voce digita: 2 2")
     print("Resultado:")
     print(cerquilha1)
-    print()
-    os.execute("pause")
-    os.execute("cls")
+
+    pausaELimpa()
     print("Essas sao todas as coordenadas")
     print()
 
     print(coordenadas)
-    print()
-    os.execute("pause")
-    os.execute("cls")
-    print("Agora estao prontos pra jogar. Bom Jogo!")
-    print()
 
-    os.execute("pause")
-    os.execute("cls")
+    pausaELimpa()
+    print("Agora estao prontos pra jogar. Bom Jogo!")
+
+    pausaELimpa()
 end
 
-function printJogo(jogo)
+function printEstadoAtualJogo(jogo)
     for i = 1, #jogo do
         for j = 1, #jogo[i] do
             local jogada = "-"
@@ -74,3 +74,35 @@ function printJogo(jogo)
         print()  -- Nova linha para cada linha da matriz
     end
 end
+
+function lerCoordenadas(jogador, jogo)
+    io.write("Jogador ", jogador, " digite suas coordenadas: ")
+    linha = io.read("*number")
+    coluna = io.read("*number")
+    
+    jogo[linha][coluna] = jogador
+    return jogo
+end
+
+function verificaValoresIguais(lista)
+    
+    local primeiroElemento = lista[1]
+    for i = 2, #lista do
+        if lista[i] ~= primeiroElemento then
+            return false
+        end
+    end
+    return true
+end
+
+
+-- function verificaGanhador(jogo)
+--     for i = 1, #jogo do
+--         local 
+--         for j = 1, #jogo[i] do
+
+--             if i == j then
+
+
+--     return 0
+-- end
